6f6125428ec4f0142200bad2dbe536e82ffb232f
Iwona <iwonajankowsky@gmail.com>
2021-05-30 16:53:47 +0200

    error handling

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -14,7 +14,7 @@ export class WeatherComponent implements OnInit {
     public dailyWeather;
     public city: City;
     public cityList: City[];
-    public showNotFoundWarning = false;
+    public error: string;
 
     constructor(
         private changeDetector: ChangeDetectorRef,
@@ -31,36 +31,50 @@ export class WeatherComponent implements OnInit {
     public getWeatherByUserCoordinates(position): void {
         const { coords: { latitude, longitude } } = position;
         this.weatherApiService.getCityByCoordinates(latitude, longitude)
-            .subscribe(cityData => {
-                this.searchWeatherByCoordinates(cityData[0]);
-            });
+            .subscribe(
+                cityData => {
+                    this.searchWeatherByCoordinates(cityData[0]);
+                },
+                err => this.handleError(err.error.message)
+            );
     }
 
     public searchWeatherByCoordinates(city: City): void {
         this.cityList = null;
 
         this.weatherApiService.getWeatherByCoordinates(city.lat, city.lon)
-            .subscribe(weather => {
-                this.city = city;
-                this.dailyWeather = weather.daily.slice(0, DAILY_WEATHER_DAYS);
-                this.changeDetector.markForCheck();
-            });
+            .subscribe(
+                weather => {
+                    this.city = city;
+                    this.dailyWeather = weather.daily.slice(0, DAILY_WEATHER_DAYS);
+                    this.changeDetector.markForCheck();
+                },
+                err => this.handleError(err.error.message)
+            );
     }
 
     public searchCityByName(cityName: string): void {
         this.dailyWeather = null;
-        this.showNotFoundWarning = false;
+        this.error = null;
 
         this.weatherApiService.getCityByName(cityName)
-            .subscribe(cityList => {
-                if (cityList.length === 0) {
-                    this.showNotFoundWarning = true;
-                } else if (cityList.length === 1) {
-                    this.searchWeatherByCoordinates(cityList[0]);
-                } else {
-                    this.cityList = cityList;
-                }
-                this.changeDetector.markForCheck();
-            });
+            .subscribe(
+                cityList => {
+                    if (cityList.length === 0) {
+                        this.handleError('Given city not found');
+                    } else if (cityList.length === 1) {
+                        this.searchWeatherByCoordinates(cityList[0]);
+                    } else {
+                        this.cityList = cityList;
+                    }
+                    this.changeDetector.markForCheck();
+                },
+                err => this.handleError(err.error.message)
+            );
+    }
+
+    private handleError(err: string): void {
+        this.error = err;
+        this.changeDetector.markForCheck();
     }
 }
